e3b68031a3162d07b3978090ec58c3223c0a41ba
RuslanTashanov18 <tashanov.2019@gmail.com>
2025-02-22 12:54:59 +0500

    полная реализация 2 методов tasktracker1

--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,7 @@ public class Test {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        TaskTracker1.addTask();
+        TaskTracker1.deleteTask();
     }
 }
